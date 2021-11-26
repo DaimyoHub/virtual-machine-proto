98d71920329dc9a71b915e77d0613f3ec9a5fd75
DaimyoHub <daimyohubcontact@gmail.com>
2021-11-26 14:40:48 +0100

    Core: Begin request queue documentation.

--- include/vm/core/request/queue.hpp
+++ include/vm/core/request/queue.hpp
@@ -1,6 +1,7 @@
 #ifndef VM_UTILITIES_REQUEST_QUEUE_HPP
 #define VM_UTILITIES_REQUEST_QUEUE_HPP
 
+#include <cassert>
 #include <mutex>
 #include <type_traits>
 #include <vector>
@@ -9,6 +10,14 @@
 
 namespace vm {
 
+/**
+ * The request queue represents a queue stocking requests dedicated to the
+ * virtual machine core. It is made to stock descriptors, so that it fits the
+ * request model design. See the request descriptor class documentation for more
+ * informations.
+ *
+ * @tparam request_descriptor_t, the request descriptor type.
+ */
 template <class request_descriptor_t>
 class RequestQueue {
  private:
@@ -40,20 +49,37 @@ class RequestQueue {
   ~RequestQueue() { requests_.clear(); }
 
  public:
+  /**
+   * Adds a request descriptor to the head of the queue.
+   *
+   */
   void enqueue(request_descriptor_t descriptor) {
     std::lock_guard<std::mutex> lock(lock_);  // lock
     requests_.emplace_back(descriptor);
   }
 
+  /**
+   * Checks if the queue is empty.
+   */
   bool is_empty() const { return requests_.empty(); }
 
+  /**
+   * Removes the request descriptor from the queue tail.
+   *
+   * @return request_descriptor_t, the request at the tail of the queue.
+   *
+   * @pre The queue must not be empty.
+   */
   request_descriptor_t dequeue() {
-    if (not is_empty()) {
-      std::lock_guard<std::mutex> lock(lock_);
-      requests_.erase(requests_.begin());
-    }
+    assert(not is_empty());
+
+    std::lock_guard<std::mutex> lock(lock_);
+    return *requests_.erase(requests_.begin());
   }
 
+  /**
+   * Gets the count of request descriptor stocked in the queue.
+   */
   std::size_t get_size() const { return requests_.size(); }
 };
 

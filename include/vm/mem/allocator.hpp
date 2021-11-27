e2882f2f614c32d89804a0a209edafcd7fd81b03
DaimyoHub <daimyohubcontact@gmail.com>
2021-11-27 11:26:05 +0100

    Memory: Add a memory allocator type.

---
+++ include/vm/mem/allocator.hpp
@@ -0,0 +1,10 @@
+#ifndef VM_MEM_ALLOCATOR_HPP
+#define VM_MEM_ALLOCATOR_HPP
+
+namespace vm {
+
+class MemoryAllocator {};
+
+}  // namespace vm
+
+#endif  // VM_MEM_ALLOCATOR_HPP
--- tests/main.cpp
+++ tests/main.cpp
@@ -4,10 +4,13 @@
 #include <vm/core/req/queue.hpp>
 #include <vm/mem/chunk.hpp>
 #include <vm/mem/device.hpp>
+#include <vm/utils/device_id.hpp>
 
 int main() {
   vm::MemoryDevice<256> mem;
   // auto test = vm::MemoryChunk(mem, mem.get_base(), 64);
 
-  vm::RequestHandler<vm::RequestKind::MEMORY> handler;
+  auto id = vm::DeviceID(vm::DeviceKind::MEMORY, 3);
+  assert(id.eval_kind() == vm::DeviceKind::MEMORY);
+  assert(id.get_id() == 3);
 }
